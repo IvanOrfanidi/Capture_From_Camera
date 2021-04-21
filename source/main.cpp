2fbb1a70c9e94c84feb1efdf34e680ccfdd68c6e
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-04-22 00:44:13 +0300

    Рефакторинг.

--- source/main.cpp
+++ source/main.cpp
@@ -3,49 +3,38 @@
 
 int main()
 {
-    //Open the default video camera
-    cv::VideoCapture cap(0);
+    // Open the default video camera.
+    cv::VideoCapture cap(cv::VideoCaptureAPIs::CAP_ANY);
 
-    // if not success, exit program
+    // If not success, exit program.
     if (cap.isOpened() == false) {
-        std::cout << "Cannot open the video camera" << std::endl;
-        std::cin.get(); //wait for any key press
+        std::cerr << "Cannot open the video camera!" << std::endl;
         return EXIT_FAILURE;
     }
 
-    double dWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-    double dHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+    const double width = cap.get(cv::CAP_PROP_FRAME_WIDTH); // Get the width of frames of the video.
+    const double height = cap.get(cv::CAP_PROP_FRAME_HEIGHT); // Get the height of frames of the video.
+    std::cout << "Resolution of the video : " << width << " x " << height << std::endl;
 
-    std::cout << "Resolution of the video : " << dWidth << " x " << dHeight << std::endl;
+    static constexpr std::string_view window_name = "My Camera";
+    cv::namedWindow(window_name.data()); // Create a window called "My Camera".
 
-    std::string window_name = "My Camera Feed";
-    cv::namedWindow(window_name); //create a window called "My Camera Feed"
-
-    while (true) {
-        cv::Mat frame;
-        bool bSuccess = cap.read(frame); // read a new frame from video
-
-        //Breaking the while loop if the frames cannot be captured
-        if (bSuccess == false) {
-            std::cout << "Video camera is disconnected" << std::endl;
-            std::cin.get(); //Wait for any key press
+    cv::Mat frame;
+    // Wait for for 10 ms until any key is pressed.
+    // If any key is not pressed withing 10 ms, continue the loop.
+    static constexpr int ESCAPE_KEY = 27;
+    static constexpr int DELAY_MS = 10;
+    while (cv::waitKey(DELAY_MS) != ESCAPE_KEY) {
+        // Read a new frame from video.
+        if (cap.read(frame) == false) { // Breaking the while loop if the frames cannot be captured.
+            std::cerr << "Video camera is disconnected!" << std::endl;
             return EXIT_FAILURE;
         }
 
-        //show the frame in the created window
-        imshow(window_name, frame);
-
-        //wait for for 10 ms until any key is pressed.
-        //If the 'Esc' key is pressed, break the while loop.
-        //If the any other key is pressed, continue the loop
-        //If any key is not pressed withing 10 ms, continue the loop
-        static constexpr int ESCAPE = 27;
-        static constexpr int DELAY = 1;
-        if (cv::waitKey(DELAY) == ESCAPE) {
-            std::cout << "Esc key is pressed by user. Stoppig the video" << std::endl;
-            break;
-        }
+        // Show the frame in the created window.
+        imshow(window_name.data(), frame);
     }
+    std::cout << "Esc key is pressed by user. Stoppig the video." << std::endl;
 
     return EXIT_SUCCESS;
 }
