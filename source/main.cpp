f75e2f437b7a74ee6bc2208f8e043143add12044
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-04-22 13:14:33 +0300

    Добавил вывод FPS.

--- source/main.cpp
+++ source/main.cpp
@@ -12,19 +12,23 @@ int main()
         return EXIT_FAILURE;
     }
 
-    const double width = cap.get(cv::CAP_PROP_FRAME_WIDTH); // Get the width of frames of the video.
-    const double height = cap.get(cv::CAP_PROP_FRAME_HEIGHT); // Get the height of frames of the video.
-    std::cout << "Resolution of the video : " << width << " x " << height << std::endl;
+    const auto width = cap.get(cv::CAP_PROP_FRAME_WIDTH); // Get the width of frames of the video.
+    const auto height = cap.get(cv::CAP_PROP_FRAME_HEIGHT); // Get the height of frames of the video.
+    std::cout << "Resolution of the video: " << width << " x " << height << "." << std::endl;
+
+    const auto fps = cap.get(cv::CAP_PROP_FPS);
+    std::cout << "Frames per seconds: " << fps << "." << std::endl;
 
     static constexpr std::string_view window_name = "My Camera";
     cv::namedWindow(window_name.data()); // Create a window called "My Camera".
 
-    cv::Mat frame;
     // Wait for for 10 ms until any key is pressed.
     // If any key is not pressed withing 10 ms, continue the loop.
     static constexpr int ESCAPE_KEY = 27;
     static constexpr int DELAY_MS = 10;
     while (cv::waitKey(DELAY_MS) != ESCAPE_KEY) {
+        cv::Mat frame;
+
         // Read a new frame from video.
         if (cap.read(frame) == false) { // Breaking the while loop if the frames cannot be captured.
             std::cerr << "Video camera is disconnected!" << std::endl;
