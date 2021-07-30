c768ce788d83076437fccfe3cb70efa64e3afca7
Ivan Orfanidi <i.orfanidi@mail.ru>
2021-07-30 23:54:55 +0300

    Рефакторинг.

--- source/main.cpp
+++ source/main.cpp
@@ -5,8 +5,6 @@ int main()
 {
     // Open the default video camera.
     cv::VideoCapture cap(cv::VideoCaptureAPIs::CAP_ANY);
-
-    // If not success, exit program.
     if (cap.isOpened() == false) {
         std::cerr << "Cannot open the video camera!" << std::endl;
         return EXIT_FAILURE;
@@ -19,13 +17,11 @@ int main()
     const auto fps = cap.get(cv::CAP_PROP_FPS);
     std::cout << "Frames per seconds: " << fps << "." << std::endl;
 
-    static constexpr std::string_view window_name = "My Camera";
-    cv::namedWindow(window_name.data()); // Create a window called "My Camera".
+    static constexpr std::string_view WINDOW_NAME = "My Camera";
+    cv::namedWindow(WINDOW_NAME.data());
 
-    // Wait for for 10 ms until any key is pressed.
-    // If any key is not pressed withing 10 ms, continue the loop.
-    static constexpr int ESCAPE_KEY = 27;
     static constexpr int DELAY_MS = 10;
+    static constexpr int ESCAPE_KEY = 27;
     while (cv::waitKey(DELAY_MS) != ESCAPE_KEY) {
         cv::Mat frame;
 
@@ -36,14 +32,9 @@ int main()
         }
 
         // Show the frame in the created window.
-        imshow(window_name.data(), frame);
+        imshow(WINDOW_NAME.data(), frame);
     }
-    std::cout << "Esc key is pressed by user. Stoppig the video." << std::endl;
-
-    // When everything done, release the video capture and write object.
     cap.release();
-
-    // Closes all the frames.
     cv::destroyAllWindows();
 
     return EXIT_SUCCESS;
