0043376418de6fb8bd21525ac3488a6086cc6675
Ivan Orfanidi <i.orfanidi@mail.ru>
2022-12-22 21:19:11 +0300

    Update.

--- source/main.cpp
+++ source/main.cpp
@@ -1,41 +1,79 @@
 ﻿#include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main()
+#include <boost/program_options.hpp>
+
+#include <project_version.h>
+
+int main(int argc, char* argv[])
 {
-    // Open the default video camera.
-    cv::VideoCapture cap(cv::VideoCaptureAPIs::CAP_ANY);
-    if (cap.isOpened() == false) {
+    std::string filename;
+    size_t customWidth;
+    size_t customHeight;
+    boost::program_options::options_description desc("options");
+    desc.add_options()
+        // All options:
+        ("filename,f", boost::program_options::value<std::string>(&filename)->default_value(""), "video file name.\n") //
+        ("width", boost::program_options::value<size_t>(&customWidth)->default_value(0), "width of the frames in the video stream.\n") //
+        ("height", boost::program_options::value<size_t>(&customHeight)->default_value(0), "height of the frames in the video stream.\n") //
+        ("version,v", "print version number and exit.\n") // Version
+        ("help,h", "produce help message.\n"); // Help
+    boost::program_options::variables_map options;
+    try {
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), options);
+        boost::program_options::notify(options);
+    } catch (const std::exception& exception) {
+        std::cerr << "error: " << exception.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (options.count("help")) {
+        std::cout << desc << std::endl;
+        return EXIT_SUCCESS;
+    }
+    if (options.count("version")) {
+        std::cout << "version: " + std::string(PROJECT_VERSION) << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    cv::VideoCapture capture;
+    if (filename.empty()) {
+        capture.open(cv::VideoCaptureAPIs::CAP_ANY);
+    } else {
+        capture.open(filename);
+    }
+
+    if (!capture.isOpened()) {
         std::cerr << "Cannot open the video camera!" << std::endl;
         return EXIT_FAILURE;
     }
 
-    const auto width = cap.get(cv::CAP_PROP_FRAME_WIDTH); // Get the width of frames of the video.
-    const auto height = cap.get(cv::CAP_PROP_FRAME_HEIGHT); // Get the height of frames of the video.
-    std::cout << "Resolution of the video: " << width << " x " << height << "." << std::endl;
+    size_t width = capture.get(cv::CAP_PROP_FRAME_WIDTH);
+    size_t height = capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+    width = (customWidth == 0) ? width : customWidth;
+    height = (customHeight == 0) ? height : customHeight;
+    const cv::Size customSize(width, height);
 
-    const auto fps = cap.get(cv::CAP_PROP_FPS);
-    std::cout << "Frames per seconds: " << fps << "." << std::endl;
+    std::cout << "Resolution of the video: " << width << "x" << height << "." << std::endl;
+    std::cout << "Frames per seconds: " << capture.get(cv::CAP_PROP_FPS) << "." << std::endl;
 
     static constexpr std::string_view WINDOW_NAME = "My Camera";
     cv::namedWindow(WINDOW_NAME.data());
 
-    static constexpr int DELAY_MS = 10;
+    static constexpr int DELAY_MS = 1;
     static constexpr int ESCAPE_KEY = 27;
     while (cv::waitKey(DELAY_MS) != ESCAPE_KEY) {
         cv::Mat frame;
-
-        // Read a new frame from video.
-        if (cap.read(frame) == false) { // Breaking the while loop if the frames cannot be captured.
+        if (!capture.read(frame)) {
             std::cerr << "Video camera is disconnected!" << std::endl;
             return EXIT_FAILURE;
         }
 
-        // Show the frame in the created window.
+        if (customWidth != 0 || customHeight != 0) {
+            cv::resize(frame, frame, customSize, 0, 0);
+        }
+
         imshow(WINDOW_NAME.data(), frame);
     }
-    cap.release();
+    capture.release();
     cv::destroyAllWindows();
-
-    return EXIT_SUCCESS;
 }
